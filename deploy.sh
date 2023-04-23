d3935c0e3394e43c36e64c2a03c4e421e4d287ce
Emerson Carvalho <carvemerson@gmail.com>
2023-04-23 17:50:24 -0300

    Deploy to gh-pages

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is used to deploy the application to the server.
-npm run build
-
-git add dist -f
-git commit -m "Deploy to gh-pages"
-git subtree push --prefix dist origin gh-pages
