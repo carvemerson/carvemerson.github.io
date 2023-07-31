7bf960d7f28998a8ff8e2913bdd0bf12423b6481
Emerson Carvalho <carvemerson@gmail.com>
2023-07-31 19:56:25 -0300

    Adjust responsivity.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="container text-center bg-light">
         <div class="container row justify-content-center align-items-center">
-            <div class="col-12 align-self-center col-sm-3 align-self-sm-end order-3 order-sm-0">
+            <div class="
+                col-12 align-self-center order-3
+                col-md-3 align-self-md-end order-md-0
+            ">
                 <div class="d-flex justify-content-center gap-2 py-3 py-sm-0 pb-sm-3">
                     <i class="bi bi-instagram"></i>
                     <i class="bi bi-github"></i>
                     <i class="bi bi-facebook"></i>
                 </div>
             </div>
-            <div class="col-6 col-sm-3 text-start">
+            <div class="col-6 col-md-3 text-start mt-3">
                 <h6>HELLO</h6>
                 <h1>I'm <span class="text-danger">Emerson</span> Carvalho</h1>
                 <h6>WEB DEVELOPER</h6>
+                <button class="btn btn-sm btn-danger text-white mt-5">Download CV</button>
             </div>
             <div class="col-6">
                 <img src="/images/person.png" alt="" class="img-fluid">
