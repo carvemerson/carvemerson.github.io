51a764391d0107818270435e3b71d660eb552d11
Emerson Carvalho <carvemerson@gmail.com>
2023-07-31 21:10:11 -0300

    WIP About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,23 @@
 <template>
     <div id="about" class="container my-5">
-        About
+        <div class="row align-items-center">
+            <div class="col-6">
+                <img src="/images/person2.png" alt="">
+            </div>
+            <div class="col-6">
+                <h1> About me </h1>
+                <p class="mt-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies mauris sed diam placerat, quis ornare massa dapibus. Vivamus eu cursus nulla. Mauris congue magna mauris, sit amet rutrum mi placerat quis. Donec sed quam et lectus auctor tincidunt vel in erat. Donec nec ullamcorper sem, sed faucibus nunc. Donec.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies mauris sed diam placerat, quis ornare massa dapibus. Vivamus eu cursus nulla.
+                </p>
+
+                <button class="mt-5 btn btn-danger text-white">
+                    Contact Me
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
