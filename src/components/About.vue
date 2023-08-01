5376c3496132e0ec62fc1391edaeca16c7b69022
Emerson Carvalho <carvemerson@gmail.com>
2023-07-31 21:30:45 -0300

    WIP About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="about" class="container my-5">
         <div class="row align-items-center">
-            <div class="col-6">
-                <img src="/images/person2.png" alt="">
+            <div class="col-12 col-sm-6">
+                <img src="/images/person.png" alt="" class="img-fluid">
             </div>
-            <div class="col-6">
+            <div class="col-12 col-sm-6">
                 <h1> About me </h1>
                 <p class="mt-5">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies mauris sed diam placerat, quis ornare massa dapibus. Vivamus eu cursus nulla. Mauris congue magna mauris, sit amet rutrum mi placerat quis. Donec sed quam et lectus auctor tincidunt vel in erat. Donec nec ullamcorper sem, sed faucibus nunc. Donec.
@@ -17,6 +17,84 @@
                     Contact Me
                 </button>
             </div>
+        </div>
+
+        <h1 class="mt-5"> My Skills </h1>
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>Languages</h3>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    HTML
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="85">
+                    <div class="progress-bar bg-danger" style="width: 85%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    CSS
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="80">
+                    <div class="progress-bar bg-danger" style="width: 80%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    PHP
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="95">
+                    <div class="progress-bar bg-danger" style="width: 95%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    JavaScript
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="90">
+                    <div class="progress-bar bg-danger" style="width: 90%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    TypeScript
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="75">
+                    <div class="progress-bar bg-danger" style="width: 75%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    MySQL
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="80">
+                    <div class="progress-bar bg-danger" style="width: 80%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    C/C++
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="60">
+                    <div class="progress-bar bg-danger" style="width: 60%"></div>
+                </div>
+            </div>
+            <div class="col-12 mt-3 col-sm-6">
+                <span>
+                    Dart
+                </span>
+                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="75">
+                    <div class="progress-bar bg-danger" style="width: 75%"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-12">
+                <h3>Frameworks</h3>
+            </div>
+            WIP
         </div>
     </div>
 </template>
